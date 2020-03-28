ea4a1396c046269d29f3d7460e3c35d7bd1f2e34
zhonghao <452180965@qq.com>
2020-03-28 16:42:07 +0800

    1订单

---
+++ pages/commodity/administration/addDetail.vue
@@ -0,0 +1,305 @@
+<template>
+	<view class="page">
+		<navbar title="新增菜品" :backcolorType='2' :showKongPanel="1" :whiteback="2"></navbar>
+		<view class="img_view">
+			<image class="food_img" src="/static/微信图片_20200318092008.jpg" mode="aspectFill" v-if="img"></image>
+			<span class="iconfont icon-jiahao1"></span>
+		</view>
+		<view class="big_view">
+			<view class="food_name">梅菜肉</view>
+			<view class="sale_num">月售100份</view>
+		</view>
+		<view class="food_introduction">
+			<view class="top_view">
+				商品简介
+			</view>
+			<view class="introduction">梅菜+肉</view>
+		</view>
+		<view class="comment_view" @click="commentClick">
+			<view class="comment">商品评论(5)</view>
+			<span class="iconfont icon-jiantou"></span>
+		</view>
+		<view class="cash_view">
+			<view class="cash">￥10.00</view>
+			<view class="btn_view">
+				<view class="collect_btn">收藏</view>
+				<view class="add_btn" @click="addGoodsClick">加入购物车</view>
+			</view>
+		</view>
+		<view class="chooseWindow_view" v-if="boxType">
+			<view class="chooseBg"></view>
+			<view class="chooseWindow">
+				<view class="choose_text">请选择</view>
+				<view class="box">
+					<view class="text">学生:</view>
+					<view class="checkbox">
+						<checkbox-group>
+							<label v-for="(item,index) in student" :key='index' @click='checkClick(item.id)'>
+								<checkbox :value="item.id" :checked='item.checked'  style="transform:scale(0.7)" /><text>{{item.name}}</text>
+							</label>
+						</checkbox-group>
+					</view>
+				</view>
+				<view class="choose_btn">添加</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import navbar from "@/components/navbar/navbar.vue"
+	export default{
+		components: {
+			navbar
+		},
+		data(){
+			return{
+				boxType:false,
+				student:[{
+					name:'周速度',
+					id: '1',
+					checked:false
+				},
+				{
+					name:'放方法',
+					id: '2',
+					checked:false
+				},
+				// {
+				// 	name:'放方法',
+				// 	id: '2',
+				// 	checked:false
+				// },
+				// {
+				// 	name:'放方法',
+				// 	id: '2',
+				// 	checked:false
+				// },
+				],
+				allId:[]
+			}
+		},
+		onLoad() {
+			
+		},
+		methods:{
+			commentClick(){
+				uni.navigateTo({
+					url:'comment/comment'
+				})
+			},
+			addGoodsClick(){
+				this.boxType = true
+			},
+			checkClick(id){
+				for(let i = 1;i<this.student.length;i++){
+					if(this.student[i].id==id){
+						this.student[i].checked = !this.student[i].checked
+						console.log('111',this.student)
+						break;
+					}
+				}
+			},
+		}
+	}
+</script>
+
+<style>
+	page{
+		color: #333333;
+	}
+	.img_view{
+		width: 100%;
+		height: 480upx;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		background: #F5F5F5;
+	}
+	.food_img{
+		width: 100%;
+		height: 100%;
+	}
+	.icon-jiahao1{
+		font-size: 80upx;
+		color: #999999;
+	}
+	.big_view{
+		padding: 20upx 24upx;
+		width: 100%;
+		box-sizing: border-box;
+	}
+	.food_name{
+		font-size: 38upx;
+		line-height: 1;
+		color: #333333;
+		font-weight: 500;
+	}
+	.sale_num{
+		font-size: 26upx;
+		line-height: 1;
+		padding-top: 30upx;
+		color: #333333;
+		font-weight: 400;
+	}
+	.cash_view{
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		box-sizing: border-box;
+		padding: 20upx 24upx;
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		z-index: 1;
+		box-shadow: 0 -10upx 20upx rgba(0,0,0,0.1);
+	}
+	.cash{
+		font-size: 36upx;
+		line-height: 1;
+		color: #FFBA59;
+		
+	}
+	.add_btn{
+		width: 168upx;
+		height: 68upx;
+		border-top-right-radius: 24upx;
+		border-bottom-right-radius: 24upx;
+		background: #FFBA59;
+		font-size: 26upx;
+		color: #FFFFFF;
+		line-height: 68upx;
+		text-align: center;
+	}
+	.top_view{
+		font-size: 38upx;
+		width: 100%;
+		padding: 20upx 24upx;
+		color: #333333;
+		border-bottom: 4upx solid #F5F5F5;
+	}
+	.introduction{
+		font-size: 26upx;
+		line-height: 1;
+		padding-top: 32upx;
+		padding-left: 24upx;
+		color: #333333;
+	}
+	.comment_view{
+		display: flex;
+		flex-direction: row;
+		align-items: flex-end;
+		justify-content: space-between;
+		padding: 60upx 24upx 24upx;
+		width: 100%;
+		border-bottom: 1upx solid #F5F5F5;
+		box-sizing: border-box;
+	}
+	.comment{
+		font-size: 38upx;
+		line-height: 1;
+		color: #333333;
+		font-weight: 400;
+		opacity: 0.9;
+	}
+	.icon-jiantou{
+		font-size: 32upx;
+		color: #333333;
+		font-weight: 400;
+		opacity: 0.9;
+	}
+	.btn_view{
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+	}
+	.collect_btn{
+		width: 140upx;
+		height: 68upx;
+		border-top-left-radius: 24upx;
+		border-bottom-left-radius: 24upx;
+		background: #FF4D4C;
+		font-size: 26upx;
+		color: #FFFFFF;
+		line-height: 68upx;
+		text-align: center;
+	}
+	.box{
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		width: 100%;
+		justify-content: center;
+		flex-wrap: wrap;
+		font-size: 26upx;
+	}
+	label{
+		padding-right: 40upx;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+	label:last-of-type{
+		padding-right: 0;
+	}
+	label text{
+		font-size: 26upx;
+	}
+	checkbox-group{
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+	}
+	.chooseBg{
+		position: fixed;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 1666upx;
+		background: #000000;
+		opacity: 0.5;
+		z-index: 100000;
+	}
+	.chooseWindow{
+		width: 600upx;
+		padding: 32upx 24upx;
+		border-radius: 24upx;
+		background: #FFFFFF;
+		z-index: 100001;
+		position: fixed;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-end;
+		top: 20%;
+		left: 50%;
+		margin-left: -300upx;
+	}
+	.choose_text{
+		color: #333333;
+		line-height: 1;
+		margin-bottom: 48upx;
+		width: 100%;
+		text-align: center;
+	}
+	.text{
+		padding-right: 80upx;
+	}
+	.chooseWindow_view{
+		z-index: 998;
+	}
+	.choose_btn{
+		width: 120upx;
+		height: 58upx;
+		font-size: 26upx;
+		color: #FFFFFF;
+		background: #FFBA59;
+		border-radius: 24upx;
+		text-align: center;
+		line-height: 58upx;
+		margin-top: 40upx;
+	}
+</style>
--- pages/commodity/administration/classify.vue
+++ pages/commodity/administration/classify.vue
@@ -21,20 +21,45 @@
 				<view class="table">操作</view>
 			</view>
 		</view>
-		<view class="table_view table_view1" v-for="(item,index) in tableList" :key='index'>
-			<view class="table_left_view">
-				<view class="table">{{item.type}}</view>
+		<block v-if="teamIndex==0">
+			<view class="table_view table_view1" v-for="(item,index) in tableList" :key='index'>
+				<view class="table_left_view">
+					<view class="table">{{item.type}}</view>
+				</view>
+				<view class="table_center_view">
+					<view class="table" v-if="item.keep==true">{{item.name}}</view>
+					<view class="table" v-if="item.keep==false"><input class="input" type="text" @input="inputClick" /></view>
+				</view>
+				<view class="table_center_view1">
+					<span class="iconfont icon-youjiantou iconTop" :class="index==0?'opacity':''" @click="topClick(index)"></span>
+					<span class="iconfont icon-youjiantou iconDown" :class="index==tableList.length - 1?'opacity':''" @click="downClick(index)"></span>
+				</view>
+				<view class="table_right_view" @click="delClick(index)">
+					<view class="table">删除</view>
+				</view>
 			</view>
-			<view class="table_center_view">
-				<view class="table">{{item.name}}</view>
+		</block>
+		<block v-if="teamIndex==1">
+			<view class="table_view table_view1" v-for="(item,index) in tableList1" :key='index'>
+				<view class="table_left_view">
+					<view class="table">{{item.type}}</view>
+				</view>
+				<view class="table_center_view">
+					<view class="table" v-if="item.keep==true">{{item.name}}</view>
+					<view class="table" v-if="item.keep==false"><input class="input" type="text" @input="inputClick1" /></view>
+				</view>
+				<view class="table_center_view1">
+					<span class="iconfont icon-youjiantou iconTop" :class="index==0?'opacity':''" @click="topClick(index)"></span>
+					<span class="iconfont icon-youjiantou iconDown" :class="index==tableList1.length - 1?'opacity':''" @click="downClick(index)"></span>
+				</view>
+				<view class="table_right_view" @click="delClick(index)">
+					<view class="table">删除</view>
+				</view>
 			</view>
-			<view class="table_center_view1">
-				<span class="iconfont icon-youjiantou iconTop" :class="index==0?'opacity':''" @click="topClick(index)"></span>
-				<span class="iconfont icon-youjiantou iconDown" :class="index==tableList.length - 1?'opacity':''"></span>
-			</view>
-			<view class="table_right_view" @click="delClick(index)">
-				<view class="table">删除</view>
-			</view>
+		</block>
+		<view class="table_btn_view">
+			<view class="table_btn table_btn1" @click="addClick">添加</view>
+			<view class="table_btn" @click="comfireClick">保存</view>
 		</view>
 	</view>
 </template>
@@ -54,6 +79,10 @@
 				tabTeamList:['大类','小类'],
 				teamIndex:0,
 				title:'大类',
+				nameValue:'',
+				nameValue1:'',
+				changeTpe: false,
+				changeTpe1: false,
 				tableList:[
 					{
 						name:'1',
@@ -72,27 +101,171 @@
 						type:'3',
 						keep:true
 					}
-				]
+				],
+				tableList1:[
+					{
+						name:'4',
+						id:'4',
+						type:'4',
+						keep:true
+					},
+					{
+						name:'5',
+						id:'5',
+						type:'5',
+						keep:true
+					},{
+						name:'6',
+						id:'6',
+						type:'6',
+						keep:true
+					}
+				],
 			}
 		},
 		onLoad() {
 			
 		},
 		methods:{
+			inputClick(e){
+				this.nameValue = e. detail.value
+			},
+			inputClick1(e){
+				this.nameValue1 = e. detail.value
+			},
 			tabTeamClick(item,index){
 				this.teamIndex = index
 				this.title = item
 			},
 			topClick(index){
-				let crr = this.tableList
-				console.log(111,crr)
-				let arr = this.tableList[index]
-				let brr = this.tableList[index - 1]
-				crr[index] = brr
-				crr[index - 1] = arr
-				this.tableList = crr
-				console.log(222,crr)
-			}
+				let crr = []
+				if(this.teamIndex==0){
+					crr = this.tableList
+				}else{
+					crr = this.tableList1
+				}
+				// console.log(crr)
+				let arr = crr.splice(index,1)
+				// console.log(arr)
+				crr.splice(index-1,0,arr[0])
+				// console.log(crr)
+				
+				if(this.teamIndex==0){
+					this.tableList = crr
+				}else{
+					this.tableList1 = crr
+				}
+			},
+			downClick(index){
+				let crr = []
+				if(this.teamIndex==0){
+					crr = this.tableList
+				}else{
+					crr = this.tableList1
+				}
+				// console.log(crr)
+				let arr = crr.splice(index,1)
+				// console.log(arr)
+				crr.splice(index+1,0,arr[0])
+				// console.log(crr)
+				
+				if(this.teamIndex==0){
+					this.tableList = crr
+				}else{
+					this.tableList1 = crr
+				}
+			},
+			delClick(index){
+				if(this.teamIndex==0){
+					this.tableList.splice(index,1)
+				}else{
+					this.tableList1.splice(index,1)
+				}
+			},
+			addClick(){
+				
+				let crr = []
+				if(this.teamIndex==0){
+					if(this.changeTpe){
+						uni.showToast({
+							title:"请先完成前一个分类",
+							icon:"none",
+							duration:1500
+						})
+						return false
+					}
+					crr = this.tableList
+					this.changeTpe = true
+				}else{
+					if(this.changeTpe1){
+						uni.showToast({
+							title:"请先完成前一个分类",
+							icon:"none",
+							duration:1500
+						})
+						return false
+					}
+					crr = this.tableList1
+					this.changeTpe1 = true
+				}
+				let arr = {
+					name:'',
+					id: crr.length + 1,
+					keep:false,
+					type:crr.length + 1
+				}
+				crr.push(arr)
+				
+				if(this.teamIndex==0){
+					this.tableList = crr
+				}else{
+					this.tableList1 = crr
+				}
+				
+			},
+			comfireClick(){
+				let crr = []
+				let arr = ''
+				if(this.teamIndex==0){
+					crr = this.tableList
+					if(this.nameValue!=''){
+						arr = this.nameValue
+					}else{
+						uni.showToast({
+							title:"分类名不能为空",
+							icon:"none",
+							duration:1500
+						})
+					}
+				}else{
+					crr = this.tableList1
+					if(this.nameValue1!=''){
+						arr = this.nameValue1
+					}else{
+						uni.showToast({
+							title:"分类名不能为空",
+							icon:"none",
+							duration:1500
+						})
+					}
+				}
+				for(let i =0;i<crr.length;i++){
+					if(crr[i].keep ==false){
+						crr[i].name = arr
+						crr[i].keep = true
+						
+						break;
+					}
+				}
+				if(this.teamIndex==0){
+					this.tableList = crr
+					this.changeTpe=false
+				}
+				else{
+					this.tableList1 = crr
+					this.changeTpe1 = false
+				}
+			},
 		}
 	}
 </script>
@@ -156,7 +329,7 @@
 		border-radius: 0;
 		background: #FFFFFF;
 	}
-	.table_view1:nth-last-of-type(2n){
+	.table_view1:nth-of-type(2n){
 		background: #C5C5C5 !important;
 	}
 	.table_left_view{
@@ -191,5 +364,35 @@
 	}
 	.opacity{
 		opacity: 0;
+	}
+	.input{
+		height: 100%;
+		width: 70%;
+		font-size: 28upx;
+		color: #333333;
+		border: 1upx solid #999;
+		margin: 0 auto;
+	}
+	/* 按钮 */
+	.table_btn_view{
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		width: 70%;
+		margin: 120upx auto 0;
+		justify-content: space-around;
+	}
+	.table_btn{
+		width: 120upx;
+		height: 58upx;
+		font-size: 26upx;
+		line-height: 58upx;
+		text-align: center;
+		border-radius: 34upx;
+		color: #FFFFFF;
+		background:linear-gradient(270deg,rgba(255,55,78,1) 1%,rgba(255,186,89,1) 100%);
+	}
+	.table_btn1{
+		background:linear-gradient(270deg,rgba(249,128,80,1) 1%,rgba(255,186,89,1) 100%) !important;
 	}
 </style>
